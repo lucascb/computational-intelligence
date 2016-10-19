81305961fc0f0cdeb38fe05d8d80796f2b14b5f0
Lucas Bernardes <lucascbernardes@live.com>
2016-10-19 00:01:16 -0200

    Exercicio 1 done

--- redes-neurais/perceptron.py
+++ redes-neurais/perceptron.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-dot_product = lambda x, y: sum([i * j for i, j in zip(x, y)])
-step = lambda x: 1 if x > 0 else 0 # Funcao degrau
+""" Segundo trabalho de Inteligencia Computacional:
+        Rede Neural Artificial Perceptron   """
 
-class Perceptron(object):
-    """ Rede Neural Artificial Perceptron """
+from random import randrange
 
-    def __init__(self, alpha, epochs):
+# Funcao degrau
+step        = lambda x: 1 if x > 0 else 0
+# Produto escalar de dois vetores
+dot_product = lambda x, y: sum([i*j for i,j in zip(x, y)])
+
+class Neuron(object):
+    """ Implementa um neuronio de uma RNA """
+
+    def __init__(self, alpha=1, epochs=100, random=False):
         self.alpha = alpha       # Taxa de aprendizagem
         self.max_epochs = epochs # Qtde max de iteracoes do alg de treinamento
-        self.theta = -1          # Threshoult
+        self.theta = -1          # Threshould
         self.w = []              # Vetor de pesos
+        self.random_w = random   # Define se o vetor de pesos sera aleat ou nao
 
     def fit(self, inputs, outputs):
-        """ Treina a RNA para o conjunto de entradas e saidas """
+        """ Treina o neuronio para o conjunto de entradas e saidas """
         if len(inputs) != len(outputs):
             raise Exception("Inputs and ouputs length should be equal")
         # Insere o bias nas entradas de treinamento
@@ -22,9 +30,12 @@ class Perceptron(object):
         for inpt in x:
             inpt.insert(0, -self.theta)
         # Cria o vetor de pesos inicial
-        self.w = [0 for i in range(len(x[0]))]
+        if self.random_w:
+            self.w = [randrange(3)-1 for i in range(len(x[0]))]
+        else:
+            self.w = [0 for i in range(len(x[0]))]
         # Inicializa as variaveis de controle
-        epoch, converged = 0, False
+        converged, epoch = False, 0
         while not converged and epoch < self.max_epochs:
             errors = [0 for i in range(len(x))]
             # Para cada entrada de treinamento
@@ -40,6 +51,7 @@ class Perceptron(object):
         print(self.w)
 
     def predict(self, x):
+        """ Testa o conhecimento do neuronio com base no vetor de pesos """
         return step(dot_product(self.w[1:], x))
 
 zero = [0, 1, 1, 1, 0,
@@ -55,3 +67,41 @@ one  = [0, 0, 1, 0, 0,
         0, 0, 1, 0, 0,
         0, 0, 1, 0, 0,
         1, 1, 1, 1, 1]
+
+two  = [0, 1, 1, 1, 0,
+        0, 1, 0, 1, 0,
+        0, 0, 0, 1, 0,
+        0, 0, 1, 0, 0,
+        0, 1, 0, 0, 0,
+        1, 1, 1, 1, 1]
+
+three = [0, 1, 1, 1, 1,
+         0, 0, 0, 0, 1,
+         0, 1, 1, 1, 1,
+         0, 0, 0, 0, 1,
+         0, 0, 0, 0, 1,
+         0, 1, 1, 1, 1]
+
+four = [1, 0, 0, 1, 0,
+        1, 0, 0, 1, 0,
+        1, 0, 0, 1, 0,
+        1, 1, 1, 1, 0,
+        0, 0, 0, 1, 0,
+        0, 0, 0, 1, 0]
+
+five = [0, 1, 1, 1, 1,
+        0, 1, 0, 0, 0,
+        0, 1, 1, 1, 1,
+        0, 0, 0, 0, 1,
+        0, 0, 0, 0, 1,
+        0, 1, 1, 1, 1]
+
+if __name__ == '__main__':
+    n = Neuron(random=True)
+    n.fit([zero, one], [0, 1])
+    print(n.predict(zero))
+    print(n.predict(one))
+    print(n.predict(two))
+    print(n.predict(three))
+    print(n.predict(four))
+    print(n.predict(five))
